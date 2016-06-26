79662ab57211e28722a5d7dccc3df230a8977ddf
mokkeee <mtg-gh-99078>
2016-06-26 21:19:47 +0900

    use count instead replace.

--- codeiq_1630.py
+++ codeiq_1630.py
@@ -3,29 +3,25 @@
 
 __author__ = 'mokkeee'
 
-import pytest
-
-import re
 from functools import reduce
 
-erase_pattern = re.compile('[0-6,8-9]+')
 
 def seven_count_in_number(num):
-    return len(erase_pattern.sub('', str(num)))
+    return str(num).count('7')
 
 
 def seven_count(max_num):
     count = reduce(
-        lambda x, y: x+y,
-        map(seven_count_in_number,
-            range(1, max_num+1)))
-    #numbers = re.sub('[0-6,8-9]+', '',
+            lambda x, y: x + y,
+            map(seven_count_in_number,
+                range(1, max_num + 1)))
+    # numbers = re.sub('[0-6,8-9]+', '',
     #    ''.join(
     #        filter(
     #            lambda s: '7' in s,
     #            map(str, range(1, max_num+1)))))
     # numbers = re.sub('[0-6,8-9]+', '',
-        # ''.join(map(str, range(1, max_num+1))))
+    # ''.join(map(str, range(1, max_num+1))))
     return count
 
 
@@ -40,6 +36,6 @@ if __name__ == '__main__':
 
 def test_count():
     assert seven_count_in_number('177') == 2
-    assert seven_count(10) == 1     # 7
-    assert seven_count(20) == 2     # 7 17
+    assert seven_count(10) == 1  # 7
+    assert seven_count(20) == 2  # 7 17
     assert seven_count(99) == 20
